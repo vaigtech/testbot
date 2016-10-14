198e411d14e2d919e4709f74788737369a54c0c9
vaigtech <C:\Users\vahe\AppData\Roaming\The Bat!>
2016-10-14 16:49:22 +0400

    d

--- index.js
+++ index.js
@@ -42,6 +42,9 @@ app.post('/webhook/', function (req, res) {
 			  case 'button':
 				sendButtonMessage(senderID);
 				break;
+				case 'receipt':
+				sendReceiptMessage(senderID);
+				break;
 /*
 			  case 'gif':
 				sendGifMessage(senderID);
@@ -63,9 +66,7 @@ app.post('/webhook/', function (req, res) {
 				sendGenericMessage(senderID);
 				break;
 
-			  case 'receipt':
-				sendReceiptMessage(senderID);
-				break;
+
 
 			  case 'quick reply':
 				sendQuickReply(senderID);
@@ -90,18 +91,7 @@ app.post('/webhook/', function (req, res) {
 			  default:
 				sendTextMessage(senderID,  "Text received, echo: "+messageText);
 			}
-/*
-			if (text === 'image') {
-		        sendImageMessage(sender);
-			}else if (text === 'button') {
-		        sendButtonMessage(sender);      
-			}else if (text === 'Generic') {
-				sendGenericMessage(sender)
-				continue
-			}else{
-				sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-			}
-*/
+
 		}
 		if (event.postback) {
 			var text = JSON.stringify(event.postback)
@@ -257,6 +247,67 @@ function sendButtonMessage(recipientId) {
       }
     }
   };  
+
+  callSendAPI(messageData);
+}
+function sendReceiptMessage(recipientId) {
+  // Generate a random receipt ID as the API requires a unique ID
+  var receiptId = "order" + Math.floor(Math.random()*1000);
+
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message:{
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "receipt",
+          recipient_name: "Peter Chang",
+          order_number: receiptId,
+          currency: "USD",
+          payment_method: "Visa 1234",        
+          timestamp: "1428444852", 
+          elements: [{
+            title: "Oculus Rift",
+            subtitle: "Includes: headset, sensor, remote",
+            quantity: 1,
+            price: 599.00,
+            currency: "USD",
+            image_url: SERVER_URL + "/assets/riftsq.png"
+          }, {
+            title: "Samsung Gear VR",
+            subtitle: "Frost White",
+            quantity: 1,
+            price: 99.99,
+            currency: "USD",
+            image_url: SERVER_URL + "/assets/gearvrsq.png"
+          }],
+          address: {
+            street_1: "1 Hacker Way",
+            street_2: "",
+            city: "Menlo Park",
+            postal_code: "94025",
+            state: "CA",
+            country: "US"
+          },
+          summary: {
+            subtotal: 698.99,
+            shipping_cost: 20.00,
+            total_tax: 57.67,
+            total_cost: 626.66
+          },
+          adjustments: [{
+            name: "New Customer Discount",
+            amount: -50
+          }, {
+            name: "$100 Off Coupon",
+            amount: -100
+          }]
+        }
+      }
+    }
+  };
 
   callSendAPI(messageData);
 }
