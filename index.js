737deae187d1626e567c0cc643dcc458adea7f53
vaigtech <C:\Users\vahe\AppData\Roaming\The Bat!>
2016-10-14 12:40:41 +0400

    ttt
    ttt

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // index
 app.get('/', function (req, res) {
-	res.send('hello world i am a secret bot1')
+	res.send('hello world i am a secret bot123')
 })
 
 // for facebook verification
