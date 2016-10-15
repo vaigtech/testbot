4fa672f581f4ed74338c7152e53278e047acaa6c
vaigtech <vaig.tech@gmail.com>
2016-10-15 14:29:10 +0400

    ;

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // index
 app.get('/', function (req, res) {
-	res.send('hello world i am a super secret bot 1.110')
+	res.send('hello world i am a super secret bot 1.111')
 })
 
 // for facebook verification
